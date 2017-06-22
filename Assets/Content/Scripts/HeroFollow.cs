949772760665affd425853fe29994d708a34a2ad
kudriavtseva <juliakudriavtseva143@gmail.com>
2017-06-22 14:06:39 +0300

    Game Design items added

---
+++ Assets/Content/Scripts/HeroFollow.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour
+{
+    public HeroPug pug;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Отримали доступ до компонентів Transform кроля та камери відповідно 
+        //(this бо скріпт застосовується до камери)
+        Transform pug_transform = pug.transform;
+        Transform camera_transform = this.transform;
+
+        //Отримали доступ до координат кролика
+        Vector3 pug_position = pug_transform.position;
+        Vector3 camera_position = camera_transform.position;
+
+        //Задаємо рух камери відповіднім до руху кроля
+        camera_position.x = pug_position.x;
+        camera_position.y  = pug_position.y;
+
+
+        //Встановлюємо координати камери
+        camera_transform.position = camera_position;
+    }
+}
---
+++ Assets/Content/Scripts/HeroPug.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroPug : MonoBehaviour
+{
+    public float speed = 1;
+    Rigidbody2D myBody = null;
+    // Use this for initialization
+    bool isGrounded = false;
+    bool JumpActive = false;
+    float JumpTime = 0f;
+    public float MaxJumpTime = 2f;
+    public float JumpSpeed = 2f;
+    void Start()
+    {
+        myBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        //[-1, 1]
+        float value = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(value) > 0)
+        {
+            Vector2 vel = myBody.velocity;
+            vel.x = value * speed;
+            myBody.velocity = vel;
+        }
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (value < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (value > 0)
+        {
+            sr.flipX = false;
+        }
+        Vector3 from = transform.position + Vector3.up * 0.3f;
+        Vector3 to = transform.position + Vector3.down * 0.1f;
+        int layer_id = 1 << LayerMask.NameToLayer("Ground");
+        //Перевіряємо чи проходить лінія через Collider з шаром Ground
+        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+        if (hit)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        //Намалювати лінію (для розробника)
+        Debug.DrawLine(from, to, Color.red);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            this.JumpActive = true;
+        }
+        if (this.JumpActive)
+        {
+            //Якщо кнопку ще тримають
+            if (Input.GetButton("Jump"))
+            {
+                this.JumpTime += Time.deltaTime;
+                if (this.JumpTime < this.MaxJumpTime)
+                {
+                    Vector2 vel = myBody.velocity;
+                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+                    myBody.velocity = vel;
+                }
+            }
+            else
+            {
+                this.JumpActive = false;
+                this.JumpTime = 0;
+            }
+        }
+    }
+}
