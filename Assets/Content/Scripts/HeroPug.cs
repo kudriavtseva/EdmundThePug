699eb51b9b641746ab9e966935136b5e0a020a13
namtab-ma-i <chuiko.vera@gmail.com>
2017-06-24 00:24:21 +0300

    Jump almost works, idle animation

--- Assets/Content/Scripts/HeroPug.cs
+++ Assets/Content/Scripts/HeroPug.cs
@@ -12,6 +12,7 @@ public class HeroPug : MonoBehaviour
     float JumpTime = 0f;
     public float MaxJumpTime = 2f;
     public float JumpSpeed = 2f;
+	
     void Start()
     {
         myBody = this.GetComponent<Rigidbody2D>();
@@ -20,66 +21,67 @@ public class HeroPug : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+		float value = Input.GetAxis ("Horizontal");
+		
+		Animator animator = GetComponent<Animator> ();
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+		Vector3 from = transform.position + Vector3.up * 0.3f;
+		Vector3 to = transform.position + Vector3.down * 0.1f;
+		int layer_id = 1 << LayerMask.NameToLayer ("Ground");
+		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+		
+		if (Mathf.Abs (value) > 0) {
+			animator.SetBool ("run", true);
+			Vector2 vel = myBody.velocity;
+			vel.x = value * speed;
+			myBody.velocity = vel;
+			
+		} else {
+			animator.SetBool ("run", false);
+		}
+		
+		if(value < 0) {
+			sr.flipX = true;
+		} else if(value > 0) {
+			sr.flipX = false;
+		}
+		
+		if(this.isGrounded) {
+			animator.SetBool ("jump", false);
+		} else {
+			animator.SetBool ("jump", true);
+		}
 
+		if(hit) {
+			Debug.Log("Cyka blyat", this);
+			isGrounded = true;
+		} else {
+			Debug.Log("Cyka blyat2222", this);
+			isGrounded = false;
+		}
+		
+		if(Input.GetButtonDown("Jump")) {
+			this.JumpActive = true;
+		}
+		if(this.JumpActive) {
+			//Якщо кнопку ще тримають
+			if(Input.GetButton("Jump")) {
+				this.JumpTime += Time.deltaTime;
+				if (this.JumpTime < this.MaxJumpTime) {
+					Vector2 vel = myBody.velocity;
+					vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+					myBody.velocity = vel;
+				}
+			} else {
+				this.JumpActive = false;
+				this.JumpTime = 0;
+			}
+		}
+		
     }
 
     void FixedUpdate()
     {
-        //[-1, 1]
-        float value = Input.GetAxis("Horizontal");
-        if (Mathf.Abs(value) > 0)
-        {
-            Vector2 vel = myBody.velocity;
-            vel.x = value * speed;
-            myBody.velocity = vel;
-        }
-
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        if (value < 0)
-        {
-            sr.flipX = true;
-        }
-        else if (value > 0)
-        {
-            sr.flipX = false;
-        }
-        Vector3 from = transform.position + Vector3.up * 0.3f;
-        Vector3 to = transform.position + Vector3.down * 0.1f;
-        int layer_id = 1 << LayerMask.NameToLayer("Ground");
-        //Перевіряємо чи проходить лінія через Collider з шаром Ground
-        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
-        if (hit)
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-        //Намалювати лінію (для розробника)
-        Debug.DrawLine(from, to, Color.red);
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            this.JumpActive = true;
-        }
-        if (this.JumpActive)
-        {
-            //Якщо кнопку ще тримають
-            if (Input.GetButton("Jump"))
-            {
-                this.JumpTime += Time.deltaTime;
-                if (this.JumpTime < this.MaxJumpTime)
-                {
-                    Vector2 vel = myBody.velocity;
-                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
-                    myBody.velocity = vel;
-                }
-            }
-            else
-            {
-                this.JumpActive = false;
-                this.JumpTime = 0;
-            }
-        }
+	
     }
 }
